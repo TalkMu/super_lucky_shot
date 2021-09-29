afff6d18508f1f561a3593b0c6865b92347b9156
yangzhuang <koolss@koolss.com>
2021-09-29 23:35:47 +0800

    tab_bar

--- src/app.config.js
+++ src/app.config.js
@@ -12,15 +12,23 @@ export default {
     navigationBarTextStyle: 'black'
   },
   tabBar:{
+    color: "#666",
+    selectedColor: "#b4282d",
+    backgroundColor: "#fafafa",
+    borderStyle: 'black',
     list:[
       {
         pagePath:"pages/home/home",
+        iconPath:"assets/tab_bar/home.png",
+        selectedIconPath:"assets/tab_bar/home_selected.png",
         text:"首页"
       },
       {
         pagePath:"pages/me/me",
+        iconPath:"assets/tab_bar/me.png",
+        selectedIconPath:"assets/tab_bar/me_selected.png",
         text:"我的"
       }
-    ]
+    ],
   }
 }
